4fdc53ab9137db1b9c6e0620e6e970bb83aaf704
Sergey_Kubatko <kubatko@vtb.ru>
2020-02-28 13:01:32 +0300

    case006 update

--- case006/src/test/java/ru/skubatko/dev/hackerrank/case006/Case006Test.java
+++ case006/src/test/java/ru/skubatko/dev/hackerrank/case006/Case006Test.java
@@ -21,14 +21,14 @@ public class Case006Test {
         };
         int[] queries = {1, 1};
 
-        int[][] actual = {
+        int[][] expected = {
                 {3, 1, 2},
                 {2, 1, 3}
         };
 
         int[][] result = swapNodes(indexes, queries);
 
-        assertThat(result).isEqualTo(actual);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -42,13 +42,13 @@ public class Case006Test {
         };
         int[] queries = {2};
 
-        int[][] actual = {
+        int[][] expected = {
                 {4, 2, 1, 5, 3}
         };
 
         int[][] result = swapNodes(indexes, queries);
 
-        assertThat(result).isEqualTo(actual);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -68,13 +68,46 @@ public class Case006Test {
         };
         int[] queries = {2, 4};
 
-        int[][] actual = {
+        int[][] expected = {
                 {2, 9, 6, 4, 1, 3, 7, 5, 11, 8, 10},
                 {2, 6, 9, 4, 1, 3, 7, 5, 10, 8, 11}
         };
 
         int[][] result = swapNodes(indexes, queries);
 
-        assertThat(result).isEqualTo(actual);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void swapNodesCase4() {
+        int[][] indexes = {
+                {2, 3},
+                {4, 5},
+                {6, -1},
+                {-1, 7},
+                {8, 9},
+                {10, 11},
+                {12, 13},
+                {-1, 14},
+                {-1, -1},
+                {15, -1},
+                {16, 17},
+                {-1, -1},
+                {-1, -1},
+                {-1, -1},
+                {-1, -1},
+                {-1, -1},
+                {-1, -1}
+        };
+        int[] queries = {2, 3};
+
+        int[][] expected = {
+                {14, 8, 5, 9, 2, 4, 13, 7, 12, 1, 3, 10, 15, 6, 17, 11, 16},
+                {9, 5, 14, 8, 2, 13, 7, 12, 4, 1, 3, 17, 11, 16, 6, 10, 15}
+        };
+
+        int[][] result = swapNodes(indexes, queries);
+
+        assertThat(result).isEqualTo(expected);
     }
 }
